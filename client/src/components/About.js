f662ac99f97e7063dff7096c6927aeb77cb40f85
miraj <miraj00@gmail.com>
2021-11-12 15:28:00 -0500

    bootcamp photo added in footer - by Miraj, Umair and Maria

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -17,7 +17,7 @@ function About() {
     
         <div className='row'>
                               
-                <p> Global-Trade LLC was established in 2021. Students from bootcamp ( as in picture below ) were put together to work on a project. 
+                <p> Global-Trade LLC was established in 2021. Students from bootcamp were put together to work on a project. 
                     While working together they realized that the Information they have learned in this project is a well supporting to move their career in Web Development Business.
                     With all that had work they had put in bootcamp and with a hope to achieve something on their own in the future, they decided to open their IT company.
                     After bootcamp completed, they all took few months of break and collected Information on establishing an LLC and doing new business start-up.
@@ -25,8 +25,8 @@ function About() {
                 <p>
                   Since then all of the team looked into expanding their resources and coming up with their best to make company efficient and profitable.
                 </p>
-   
-                {/* <div> <img className='company' src={ClassTeam} /> </div> */}
+                <div> Click Here To see Our Bootcamp Family :<a href="https://github.com/miraj00/global-trade/blob/main/client/src/assets/images/Class-Team.JPG ">Link</a></div>
+              
 
        </div>
     </div>   
@@ -37,3 +37,6 @@ function About() {
     }   
 
 export default About;
+
+
+
