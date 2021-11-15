6da6c560c0e64186d9718a26a9fb927c590bd998
Jose Mejia <mejialaguna@yahoo.com>
2021-11-14 22:42:12 -0500

    privacy-modal-by-josemejia&&skyllerByron

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -38,7 +38,33 @@ function ModalC(props) {
           </Modal.Footer>
         </Modal>
       ) : (
-        
+        <Modal
+          {...props}
+          show={isModalOpen}
+          onHide={onClose}
+          animation={true}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header >
+            <Modal.Title id="contained-modal-title-vcenter">
+              PRIVACY GUIDE
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body show={isModalOpen} onHide={onClose} animation={true}>
+            <p>
+              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
+              ac consectetur ac, vestibulum at eros.
+            </p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button style={display.variant} onClick={onClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
       )}
     </div>
   );
